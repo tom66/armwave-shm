46590a5d5086d55477664e254e7f8e21f2ac50a1
Tom Oldbury <toldbury@gmail.com>
2020-07-01 21:41:29 +0100

    Debugging

--- xvtest.c
+++ xvtest.c
@@ -302,8 +302,8 @@ int main (int argc, char* argv[]) {
     while (frames < 200) {	
       for (i = 0; i < yuv_image->height; i++) {
         for (j = 0; j < yuv_image->width; j += 4) {
-          yuv_image->data[yuv_image->width*i + j + 0] = num;  
-          yuv_image->data[yuv_image->width*i + j + 1] = 0x00;
+          yuv_image->data[yuv_image->width*i + j + 0] = 0xff;  
+          yuv_image->data[yuv_image->width*i + j + 1] = 0xff;
           yuv_image->data[yuv_image->width*i + j + 2] = 0x00;
           yuv_image->data[yuv_image->width*i + j + 3] = 0x00;
         }
