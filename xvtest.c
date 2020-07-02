7a71b0071dc2fe7ed2b274493920f360978653ac
Tom Oldbury <toldbury@gmail.com>
2020-07-02 12:01:04 +0100

    Debug

--- xvtest.c
+++ xvtest.c
@@ -334,8 +334,8 @@ int main (int argc, char* argv[]) {
     for (i = 0; i < yuv_image->height; i += 1) {
       for (j = 0; j < yuv_image->width; j += 1) {
         rgb_col.r = i;
-        rgb_col.g = j;
-        rgb_col.b = num;
+        rgb_col.g = 0;
+        rgb_col.b = 0;
         rgb2yuv(&rgb_col, &yuv_col);
         
         //yuv_col.y = num;
