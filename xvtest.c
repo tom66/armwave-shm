a63d62349b75e90bf1f195086c4270bdabbe0164
Tom Oldbury <toldbury@gmail.com>
2020-07-02 15:22:34 +0100

    Debug

--- xvtest.c
+++ xvtest.c
@@ -53,7 +53,7 @@ void plot_pixel_yuv(XvImage *img, int x, int y, struct yuv_t *yuv_in)
     
     img->data[(img->width * y) + x] = yuv_in->y; 
     img->data[img->offsets[1] + (img->pitches[1] * y) + (x / 2)] = yuv_in->v;
-    img->data[img->offsets[2] + (img->pitches[2] * y) + (x / 2)] = yuv_in->u;
+    //img->data[img->offsets[2] + (img->pitches[2] * y) + (x / 2)] = yuv_in->u;
 }
 
 int main (int argc, char* argv[]) {
