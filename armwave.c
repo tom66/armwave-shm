5b479c507fd7ac6308102b43fa3c8b446c831bee
Tom Oldbury <toldbury@gmail.com>
2020-07-03 12:40:26 +0100

    Debug

--- armwave.c
+++ armwave.c
@@ -1058,7 +1058,9 @@ int main()
         if(num % stat_rate == 0) {
             end = clock();
             time_elapsed = ((float)(end - start)) / CLOCKS_PER_SEC;
-            printf("%d frames (%6d total) took %.2f ms (%.1f fps)\n", stat_rate, num, time_elapsed * 1000, ((float)stat_rate) / time_elapsed);
+            printf("%d frames (%6d total) took %.2f ms (%.1f fps, %d waves/sec)\n", \
+                stat_rate, num, time_elapsed * 1000, ((float)stat_rate) / time_elapsed,
+                (((float)stat_rate) / time_elapsed) * armwave_state.waves_max);
             
             start = clock();
         }
