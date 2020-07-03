5bac6b2b6d0d20d9746398cf01973464c71166e4
Tom Oldbury <toldbury@gmail.com>
2020-07-03 11:38:12 +0100

    Debug

--- armwave.c
+++ armwave.c
@@ -1010,33 +1010,18 @@ int main()
         
         armwave_fill_xvimage_scaled(yuv_image);
         
-#if 1
-        for(j = 32; j < yuv_image->height; j += 32) {
-            printf("Render H-line %d\n", j);
-            draw_horiz_line_fast_rgb_xvimage(yuv_image, 0, num % yuv_image->width, j, &grat_rgb_col);
-        } 
-        
-        for(j = 32; j < yuv_image->width; j += 31) {
-            printf("Render V-line %d\n", j);
-            draw_vert_line_fast_rgb_xvimage(yuv_image, j, 0, num % yuv_image->height, &grat_rgb_col);
-        } 
-#endif
-
         num += 1;
         XGetGeometry(dpy, window, &_dw, &_d, &_d, &_w, &_h, &_d, &_d);
         
         XvShmPutImage(dpy, xv_port, window, gc, yuv_image,
             0, 0, yuv_image->width, yuv_image->height,
-            0, 0, _w / 2, _h, True);
-        
-        /*
-        XvShmPutImage(dpy, xv_port, window, gc, yuv_image,
-            0, 0, yuv_image->width, yuv_image->height,
-            200, 200, _w / 2, _h, True);
-        */
+            0, 0, _w, _h, True);
         
         XSetForeground(dpy, gc, grat_colour.pixel);
-        XDrawLine(dpy, window, gc, 0, 10, yuv_width, 10);
+        
+        for(i = 0; i < (_w / 12); i++) {
+            XDrawLine(dpy, window, gc, (_w / 12) * i, 0, (_w / 12) * i, _h);
+        }
         
         /* XFlush(dpy); */
          
