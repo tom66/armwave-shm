3b78ab5ec1e6801ad5be8097a197409dced04c17
Tom Oldbury <toldbury@gmail.com>
2020-07-03 21:02:06 +0100

    Debug

--- armwave.c
+++ armwave.c
@@ -872,7 +872,21 @@ int main()
 			 mask, &xswa);
     
     printf("X11 Window: %d (0x%08x)\n", window, window);
-    armwave_grab_xid(window);
+    
+    
+    XStoreName(dpy, window, "ArmWave");
+    XSetIconName(dpy, window, "ArmWave");
+    XSelectInput(dpy, window, StructureNotifyMask);
+    
+    XMapWindow(dpy, window);
+    
+    do {
+        XNextEvent(dpy, &event);
+    }
+    while (event.type != MapNotify || event.xmap.event != window);
+    
+    g_window = window;
+    //armwave_grab_xid(window);
     
     /*
      * Try to strip decoration from window.
