23e38fc435130684a1a7082b65fae18be3e6974a
Tom Oldbury <toldbury@gmail.com>
2020-07-03 12:49:52 +0100

    Debug

--- armwave.c
+++ armwave.c
@@ -324,9 +324,15 @@ void render_nonaa_to_buffer_1ch_slice(uint32_t slice_y, uint32_t height)
                 word0 >>= 8;
             }
             
+            word1 = (word >> 32) & 0xffffffff;
+
             for(ys = 4; ys < 8; ys++) {
                 scale_value = word1 & 0xff;
                 
+                // Keep math in integer where possible.  We compute the X scale and then multiply to get the correct 
+                // base coordinate.  The value of the point then informs us where to write in typically an 8-bit window.
+                // The bonus of this method is that we tend to hit accesses along a 256 byte line.  (512 byte lines if
+                // we set our accumulation buffer to 16 bits.)
                 write_buffer = write_buffer_base + \
                     ((((yy + ys) * g_armwave_state.cmp_x_bitdepth_scale) >> AM_XCOORD_MULT_SHIFT) * 256 * sizeof(bufftyp_t));
 
