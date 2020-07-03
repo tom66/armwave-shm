f520ebbbb22f854a679b33bace3a3e0b683289b3
Tom Oldbury <toldbury@gmail.com>
2020-07-03 22:17:56 +0100

    Debug

--- armwave.c
+++ armwave.c
@@ -817,7 +817,7 @@ void armwave_render_frame_x11()
 {
     Window _dw;
     int _d, _w, _h;
-    int m = g_armwave_state.frame_margin + 1; 
+    int m = g_armwave_state.frame_margin; 
     
     armwave_set_wave_pointer_as_testbuf(g_frame_num % g_n_test_waves);
     armwave_generate();
