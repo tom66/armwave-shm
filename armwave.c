dc89fe8f5307dbaed927ec94fedee5029c70198f
Tom Oldbury <toldbury@gmail.com>
2020-07-03 22:18:16 +0100

    Debug

--- armwave.c
+++ armwave.c
@@ -835,7 +835,7 @@ void armwave_render_frame_x11()
     
     XvShmPutImage(g_dpy, g_xv_port, g_window, g_gc, g_yuv_image,
         0, 0, g_yuv_image->width, g_yuv_image->height,
-        m, m, _w - (m * 2), _h - (m * 2), True);
+        m, m, _w - (m * 4), _h - (m * 4), True);
     
     armwave_render_graticule();
 }
